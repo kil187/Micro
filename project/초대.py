eaecf35cfc30fe95a3c32b207531a5996a505fac
sion <63771691+kil187@users.noreply.github.com>
2022-06-21 01:49:52 +0900

    Update 초대.py

--- project/초대.py
+++ project/초대.py
@@ -9,8 +9,8 @@ class Slash(commands.Cog):
     @cog_ext.cog_slash(name="초대", description='봇초대')
     async def help(self, ctx: SlashContext):
         embed = discord.Embed(title="Micro Bot 초대")
-        embed.add_field(name='관리자 ⭕', value='[초대장](https://discord.com/api/oauth2/authorize?client_id=987739459237838908&permissions=8&scope=bot%20applications.commands)')
-        embed.add_field(name='관리자 ❌', value='[초대장](https://discord.com/api/oauth2/authorize?client_id=987739459237838908&permissions=0&scope=bot%20applications.commands)')
+        embed.add_field(name='관리자 ⭕', value='[초대장](봇초대링크)')
+        embed.add_field(name='관리자 ❌', value='[초대장](봇초대링크)')
         await ctx.send(content="",embeds=[embed])
 
 def setup(bot):
